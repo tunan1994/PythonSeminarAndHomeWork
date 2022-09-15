d850457e4236fff41fb8a2230beaa81bf35a70bd
tunan1994 <tunan@mail.ru>
2022-09-16 01:04:50 +0300

    add234

--- HomeWork3/Task2.py
+++ HomeWork3/Task2.py
@@ -7,3 +7,16 @@
 # [2, 3, 4, 5, 6] => [12, 15, 16];
 # [2, 3, 5, 6] => [12, 15]
 
+import random
+n = int(input('Введите длину массива: '))
+list = [random.randint(1, 45) for i in range(n)]
+print(list)
+
+if len(list) % 2 == 1:
+    n1 = len(list) // 2 +1
+else:
+    n1 = len(list)//2
+list1 = []
+for i in range(n1):
+    list1.append(list[i]*list[n-i-1])
+print(list1)
--- HomeWork3/Task3.py
+++ HomeWork3/Task3.py
@@ -7,3 +7,14 @@
 
 # [1.1, 1.2, 3.1, 5.17, 10.02] => 0.18 или 18
 # [4.07, 5.1, 8.2444, 6.98] - 0.91 или 91
+
+
+import random
+n = int(input('Введите длину массива: '))
+list = [random.randint(1, 45)/1.01 for i in range(n)]
+print(list)
+
+b =[]
+for i in list:
+    b = b + [i - (i * 10 // 10)]
+print(f'Разница: {round(max(b) - min(b), 2)}')
