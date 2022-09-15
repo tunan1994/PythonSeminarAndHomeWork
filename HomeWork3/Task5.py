e0a860267bd78b7a2397dd7061803171eb875f71
tunan1994 <tunan@mail.ru>
2022-09-16 01:14:13 +0300

    add5

--- HomeWork3/Task5.py
+++ HomeWork3/Task5.py
@@ -7,9 +7,25 @@
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
 
-def Fib(n):
-    if n <= 1:
-        return n
+a = int(input("Введите число:"))
+
+def nefib(n):
+    if n == -1:
+        return 1
+    elif n == -2:
+        return -1
     else:
-        return Fib((-1)**(n+1))*Fib
-print(Fib(12))
+        return nefib(n+2)-nefib(n+1)
+
+def fib(n):
+    if n in [1,2]:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+list = []
+for i in range(1,a+1):
+    list.insert(0,(pow(-1,i+1)*fib(i)))
+    list.append(fib(i))
+list.insert(a,0)
+print(list)
